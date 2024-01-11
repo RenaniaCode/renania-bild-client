72e6c73823ad68916f3303ce9fe20ccef8bd338f
Carlos A Perez <carlosa.ps@outlook.com>
2024-01-10 19:51:09 -0600

    details fixed, need to fix the component navBar to delete the iseconf if option

--- src/components/Web/HomePage/SideBar/SideBar.js
+++ src/components/Web/HomePage/SideBar/SideBar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./SideBar.scss";
-import BlackVersionLogo from "../../../../assets/Images/Logo/BlackVersionNoBackground.png";
 import { useLocation } from "react-router-dom";
 
 export function SideBar(props) {
-  const {active, toggle} = props
+  const { toggle } = props
   const location = useLocation().pathname;
   return (
     <nav id="navSidebar">
         <div id="navSideMenu">
-          <a href="/Galleries/Potrait" ><span className="navSideOptions">Servicios</span></a>
-          <a href="/AboutMe"><span className="navSideOptions" >Sobre Mi</span></a>
+          <a href="/" onClick={toggle}><span className="navSideOptions">Home</span></a>
+          <a href={location !== "/" ? "/Galleries/Potrait" : "#servicesBox"} onClick={toggle}><span className="navSideOptions">Servicios</span></a>
+          <a href="/AboutMe" onClick={toggle}><span className="navSideOptions" >Sobre Mi</span></a>
+          <a href="#footerBox" onClick={toggle}><span className="navSideOptions">Contacto</span></a>
         </div>
     </nav>
   );
